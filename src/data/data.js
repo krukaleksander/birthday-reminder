12954eefbcccec91d729e2d1d285087908ddd258
Alek Kruk <kruk.aleksander@hotmail.com>
2020-12-22 18:27:32 +0100

    create data folder, create data.js

---
+++ src/data/data.js
@@ -0,0 +1,33 @@
+const persons = [{
+        id: 1,
+        name: 'Aleksandra Szczepanik',
+        age: 29,
+        image: 'https://res.cloudinary.com/diqqf3eq2/image/upload/v1595959131/person-2_ipcjws.jpg',
+    },
+    {
+        id: 2,
+        name: 'Wojciech Mazgaj',
+        age: 32,
+        image: 'https://res.cloudinary.com/diqqf3eq2/image/upload/v1595959131/person-3_rxtqvi.jpg',
+    },
+    {
+        id: 3,
+        name: 'Krzysztof Malaga',
+        age: 36,
+        image: 'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883423/person-4_t9nxjt.jpg',
+    },
+    {
+        id: 4,
+        name: 'Grzegorz Baranica',
+        age: 34,
+        image: 'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883417/person-3_ipa0mj.jpg',
+    },
+    {
+        id: 5,
+        name: 'Waleria Mocna',
+        age: 29,
+        image: 'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883334/person-1_rfzshl.jpg',
+    },
+];
+
+export default persons;
